a6ec0019b6b83b9fdc4deb1216c1204d03afec8d
gomgomi <jhbaessi.dev@gmail.com>
2020-07-30 13:15:47 +0900

    tag 목록이 표시될 Accordion 컴포넌트 추가

--- frontend/src/tagFilter.tsx
+++ frontend/src/tagFilter.tsx
@@ -1,31 +1,75 @@
-import React from "react";
-import { Container, Row, ToggleButton } from "react-bootstrap"
+import React, { useState } from "react";
+import { Accordion, Card, useAccordionToggle } from "react-bootstrap";
+import { ChevronDown, ChevronUp } from "react-bootstrap-icons";
+
+enum Tags {
+  All,
+  FashionAndBeauty,
+  FoodAndEating,
+  Entertainment,
+  VlogAndDaily,
+  ASMR,
+  Game,
+  FetAndAnimal,
+  ITAndTechnology,
+  MovieAndAnime,
+  Automobile,
+  Music,
+  Sport,
+  Fun,
+  NewsAndPolitical,
+  Education,
+  SocialAndReligion,
+  Kids,
+  Etc,
+};
+
+interface AccodionToggleButtonProps {
+  children: any,
+  eventKey: string,
+  isExpanded: boolean,
+  handleAccordionToggle: any
+};
+
+const AccodionToggleButton = (props: AccodionToggleButtonProps) => {
+  const decoratedOnClick = useAccordionToggle(props.eventKey, props.handleAccordionToggle);
+
+  return (
+    <button
+      type="button"
+      style={{ backgroundColor: "pink" }}
+      onClick={decoratedOnClick}
+    >
+    {props.isExpanded ? <ChevronUp/> : <ChevronDown/>}
+    </button>
+  );
+};
 
 const TagFilter = () => {
-    return(
-        <Container>
-            <Row>   
-                <ToggleButton
-                    type="checkbox"
-                    variant="primary"
-                    value="1">
-                        All
-                </ToggleButton>
-                <ToggleButton
-                    type="checkbox"
-                    variant="primary"
-                    value="2">
-                        Tag A
-                </ToggleButton>
-                <ToggleButton
-                    type="checkbox"
-                    variant="primary"
-                    value="3">
-                        Tag B
-                </ToggleButton>
-            </Row>
-        </Container>
-    );
-}
+  const [isExpanded, setExpanded] = useState(false);
+
+  const toggledTagList = () => {
+    setExpanded(!isExpanded);
+  };
+
+  return (
+    <Accordion>
+      <Card>
+        <Card.Header>
+          Tags
+          <AccodionToggleButton
+            eventKey="0"
+            isExpanded={isExpanded}
+            handleAccordionToggle={toggledTagList}
+            >
+          </AccodionToggleButton>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>Tags 2</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+  );
+};
 
 export default TagFilter;
