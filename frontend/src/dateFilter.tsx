0c03e10aaa54baf087a3c5e2ce1607e6fedba58c
gomgomi <jhbaessi.dev@gmail.com>
2020-08-05 15:22:34 +0900

    tag & date filter 스타일시트 수정

--- frontend/src/dateFilter.tsx
+++ frontend/src/dateFilter.tsx
@@ -1,34 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Dropdown, Col } from "react-bootstrap";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 const DateFilter = () => {
-    const handleChange = () => {
+  const [date, setDate] = useState(new Date());
 
-    }
+  const handleChange = (date: Date) => {
+    setDate(date);
+  };
 
-    return(
-        <Container>
-            <Row>
-                <Col>
-                    <DatePicker onChange={handleChange}/>
-                </Col>
-                <Col>
-                    <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic">
-                            Daily
-                        </Dropdown.Toggle>
+  return (
+    <Container className="panel-filter">
+      <Row>
+        <Col>
+          <DatePicker selected={date} onChange={handleChange} />
+        </Col>
+        <Col>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              Daily
+            </Dropdown.Toggle>
 
-                        <Dropdown.Menu>
-                            <Dropdown.Item href="#/action-1">Daily</Dropdown.Item>
-                            <Dropdown.Item href="#/action-2">Weekly</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </Col>
-            </Row>
-        </Container>
-    );
-}
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Daily</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Weekly</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default DateFilter;
