018537b4d215fee0753292fcf42e2c37f98f83c0
gomgomi <jhbaessi.dev@gmail.com>
2020-08-07 01:50:22 +0900

    resolve #8 - 컨텐츠 컴포넌트와 컨텐츠 목록 추가

--- frontend/src/contentList.tsx
+++ frontend/src/contentList.tsx
@@ -1,26 +1,50 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Card } from "react-bootstrap";
 
-const ContentList = () => {
-    return(
-        <Container>
-            <Row>
-                <Col>Content</Col>
-                <Col>Content</Col>
-                <Col>Content</Col>
-            </Row>
-            <Row>
-                <Col>Content</Col>
-                <Col>Content</Col>
-                <Col>Content</Col>
-            </Row>
-            <Row>
-                <Col>Content</Col>
-                <Col>Content</Col>
-                <Col>Content</Col>
-            </Row>
-        </Container>
-    );
+interface ContentItemProps {
+  thumbnail: string,
+  date: any,
+  title: string,
+  hits: number,
 }
+
+/**
+ * ContentList에 표시되는 아이템 컴포넌트
+ */
+// const ContentItem = (props: ContentItemProps) => {
+const ContentItem = () => {
+  return (
+    <Card className="content">
+      <Card.Img variant="top" src={require("./assets/test-content-thumbnail.jpg")}/>
+      <Card.Body>
+        <Card.Text>date</Card.Text>
+        <Card.Text>title</Card.Text>
+        <Card.Text>hits</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+interface ContentListProps {
+
+}
+
+/**
+ * 서버로부터 획득한 컨텐츠 리스트를 표시하는 컴포넌트
+ */
+const ContentList = () => {
+  let contents = [];
+  for (let i = 0; i < 32; i ++) {
+    contents.push(<ContentItem key={i} />);
+  }
+
+  return (
+    <Container>
+      <Row>
+        {contents}
+      </Row>
+    </Container>
+  );
+};
 
 export default ContentList;
