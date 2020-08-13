621532aa88a2ab70ec91b89d297561f9f1fd87b3
gomgomi <jhbaessi.dev@gmail.com>
2020-08-14 00:34:51 +0900

    Load more 버튼으로 항목 표시되도록 우선 구현

--- frontend/src/contentList.tsx
+++ frontend/src/contentList.tsx
@@ -1,48 +1,85 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Row, Card } from "react-bootstrap";
+import InfiniteScroll from "react-infinite-scroller";
 
 interface ContentItemProps {
-  thumbnail: string,
-  date: any,
-  title: string,
-  hits: number,
-}
+  thumbnail: string;
+  date: string;
+  title: string;
+  hits: number;
+};
 
 /**
  * ContentList에 표시되는 아이템 컴포넌트
  */
 // const ContentItem = (props: ContentItemProps) => {
-const ContentItem = () => {
+const ContentItem = (props: ContentItemProps) => {
   return (
     <Card className="content">
-      <Card.Img variant="top" src={require("./assets/test-content-thumbnail.jpg")}/>
+      <Card.Img variant="top" src={props.thumbnail} />
       <Card.Body>
-        <Card.Text>date</Card.Text>
-        <Card.Text>title</Card.Text>
-        <Card.Text>hits</Card.Text>
+        <Card.Text>{props.date}</Card.Text>
+        <Card.Text>{props.title}</Card.Text>
+        <Card.Text>{props.hits}</Card.Text>
       </Card.Body>
     </Card>
   );
 };
 
-interface ContentListProps {
-
-}
+interface ContentListProps {}
 
 /**
  * 서버로부터 획득한 컨텐츠 리스트를 표시하는 컴포넌트
  */
 const ContentList = () => {
-  let contents = [];
-  for (let i = 0; i < 32; i ++) {
-    contents.push(<ContentItem key={i} />);
-  }
+  const [contents, setContents] = useState<JSX.Element[]>([]);
+  const [hasMoreContents, setHasMoreContents] = useState(true);
+
+  const loadFunc = () => {
+    const newContents  = Object.assign([], contents);
+    const prevContentCount = newContents.length;
+
+    let todayDate = new Date();
+    let date = String(todayDate.getFullYear());
+    date += "-";
+    date += String(todayDate.getMonth() + 1).padStart(2, '0');
+    date += "-";
+    date += String(todayDate.getDate()).padStart(2, '0');
+
+    for (let i = 0; i < 10; i++) {
+      newContents.push(
+        <ContentItem
+          key={prevContentCount + i}
+          thumbnail={require("./assets/test-content-thumbnail.jpg")}
+          date={date}
+          title="Test title"
+          hits={prevContentCount + i}
+        />
+      );
+    }
+
+    setContents(newContents);
+  };
+
+  useEffect(() => {
+    console.log("Called useEffect");
+    console.log(contents);
+  }, [contents]);
 
   return (
     <Container>
-      <Row>
+      {/* <InfiniteScroll
+        pageStart={0}
+        loadMore={loadFunc}
+        hasMore={hasMoreContents}
+        loader={<div className="loader">Loading ...</div>}
+        // useWindow={false}
+        // getScrollParent={() => this.scrollParentRef}
+      >
         {contents}
-      </Row>
+      </InfiniteScroll> */}
+      <Row className="contents">{contents}</Row>
+      <Row><button onClick={loadFunc}>Load more</button></Row>
     </Container>
   );
 };
